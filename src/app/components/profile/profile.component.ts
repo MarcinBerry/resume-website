636a9827ce3c8857977092bc81eb4b6e9d1982ff
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-09-24 14:27:56 +0200

    Naprawy błędów, uzupełnienie informacji, optymalizacja mediów

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'profile',
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  // @ts-ignore
+  innerWidth: number;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
+  isMobile(): boolean {
+    return this.innerWidth < 815;
   }
 }
